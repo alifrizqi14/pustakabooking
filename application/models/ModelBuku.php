07dc96708d3a90e9027d6b67330be3fb57e79d98
Alif Rizqi Mulyawan <alif.aqm@bsi.ac.id>
2021-11-23 15:09:27 +0700

    Membuat Form Login
    Membuat form login dengan membagi menjadi 3 bagian template

--- application/models/ModelBuku.php
+++ application/models/ModelBuku.php
@@ -73,7 +73,10 @@ class ModelBuku extends CI_Model{
     public function joinKategoriBuku($where)
     {
         $this->db->select('buku.id_kategori,kategori.kategori');
-        
+        $this->db->from('buku');
+        $this->db->join('kategori','kategori.id=buku.id_kategori');
+        $this->db->where($where);
+        return $this->db->get();
     }
 
 }
